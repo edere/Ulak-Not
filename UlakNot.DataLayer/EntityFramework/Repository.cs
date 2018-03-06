299d8c4747a5cb5c36678eba21024a7cfa219c24
Hüseyin Burak Kocabıyık <hbkocabiyik@gmail.com>
2018-03-06 04:18:52 +0300

    Design Pattern'ları oluşturuldu - Repository ve Singleton Pattern'ları.

---
+++ UlakNot.DataLayer/EntityFramework/Repository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlakNot.DataLayer.EntityFramework
+{
+    public class Repository<T> : Singleton where T : class
+    {
+        private DbSet<T> dbset;
+
+        public Repository()
+        {
+            dbset = context.Set<T>();
+        }
+
+        public List<T> List()
+        {
+            return dbset.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> where)
+        {
+            return dbset.Where(where).ToList();
+        }
+
+        public int Save()
+        {
+            return context.SaveChanges();
+        }
+
+        public int Insert(T obj)
+        {
+            dbset.Add(obj);
+            return Save();
+        }
+
+        public int Update(T obj)
+        {
+            return Save();
+        }
+
+        public int Delete(T obj)
+        {
+            dbset.Remove(obj);
+            return Save();
+        }
+
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return dbset.FirstOrDefault(where);
+        }
+    }
+}
---
+++ UlakNot.DataLayer/EntityFramework/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UlakNot.DataLayer.EntityFramework
+{
+    public class Singleton
+    {
+        protected static DatabaseContext context;
+        protected static object _contextLock = new object();
+
+        private static void CreateContext()
+        {
+            if (context == null)
+            {
+                lock (_contextLock)
+                {
+                    if (context == null)
+                    {
+                        context = new DatabaseContext();
+                    }
+                }
+            }
+        }
+
+        protected Singleton()
+        {
+            CreateContext();
+        }
+    }
+}
