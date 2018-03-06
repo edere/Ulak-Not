bd4bc03c95dc25c247edc759a986974945f4df8d
erenc3 <huseyinerencomert@gmail.com>
2018-03-06 15:42:45 +0300

    Yönetim paneli arayüz işlemleri gerçekleştirildi.

---
+++ UlakNot.Web/Controllers/AdministratorController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UlakNot.Web.Controllers
+{
+    public class AdministratorController : Controller
+    {
+        // GET: Administrator
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
