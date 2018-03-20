2e95df304d5bf4f72218fc3b6ae4dff7ac25db82
erenc3 <huseyinerencomert@gmail.com>
2018-03-20 16:19:00 +0300

    Admin Login İşlemi Gerçekleştirildi.

--- UlakNot.Web/Controllers/AdministratorController.cs
+++ UlakNot.Web/Controllers/AdministratorController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UlakNot.BusinessLayer.Control;
+using UlakNot.BusinessLayer.Results;
+using UlakNot.Entity;
+using UlakNot.Entity.UserObjects;
 
 namespace UlakNot.Web.Controllers
 {
@@ -12,6 +16,27 @@ namespace UlakNot.Web.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager um = new UserManager();
+                ErrorResult<UnUsers> res = um.LoginUser(model);
+
+                if (res.Error.Count > 0)
+                {
+                    res.Error.ForEach(x => ModelState.AddModelError("", x));
+                    return View(model);
+                }
+
+                Session["login"] = res.Result;
+                return RedirectToAction("Index", "Administrator");
+            }
+
+            return View(model);
         }
 
         public ActionResult Index()
