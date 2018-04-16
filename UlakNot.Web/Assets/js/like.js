5522bc5cc5f231bd57735f6655c6a23eda78a082
Hüseyin Burak Kocabıyık <hbkocabiyik@gmail.com>
2018-04-16 13:11:06 +0300

    21 - Not beğenme işlemleri için javascriptler controllere göre oluşturuldu.

--- UlakNot.Web/Assets/js/like.js
+++ UlakNot.Web/Assets/js/like.js
@@ -1,4 +1,4 @@
-$(function () {
+ï»¿$(function () {
     var noteids = [];
 
     $("div[data-note-id]").each(function (i, e) {
@@ -15,11 +15,11 @@ $(function () {
                 var id = data.result[i];
                 var likedNote = $("div[data-note-id=" + id + "]");
                 var btn = likedNote.find("button[data-liked]");
-                var span = btn.find("span.icon");
+                var span = btn.find("span.like-star");
 
                 btn.data("liked", true);
-                span.removeClass("fa-thumbs-o-up");
-                span.addClass("fa-thumbs-up");
+                span.removeClass("glyphicon-star-empty");
+                span.addClass("glyphicon-star");
             }
         }
     }).fail(function () {
@@ -29,7 +29,7 @@ $(function () {
         var btn = $(this);
         var liked = btn.data("liked");
         var noteid = btn.data("note-id");
-        var spanStar = btn.find("span.icon");
+        var spanStar = btn.find("span.like-star");
         var spanCount = btn.find("span.like-count");
 
         console.log(liked);
@@ -51,17 +51,17 @@ $(function () {
 
                 console.log("like count(after) : " + spanCount.text());
 
-                spanStar.removeClass("fa-thumbs-o-up");
-                spanStar.removeClass("fa-thumbs-up");
+                spanStar.removeClass("glyphicon-star-empty");
+                spanStar.removeClass("glyphicon-star");
 
                 if (liked) {
-                    spanStar.addClass("fa-thumbs-up");
+                    spanStar.addClass("glyphicon-star");
                 } else {
-                    spanStar.addClass("fa-thumbs-o-up");
+                    spanStar.addClass("glyphicon-star-empty");
                 }
             }
         }).fail(function () {
-            alert("Sunucu ile baðlantý kurulamadý.");
+            alert("Sunucu ile baÄŸlantÄ± kurulamadÄ±.");
         })
     });
 });
