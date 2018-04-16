3128b0c9e2b7b7bbf1af43a3b2e956d810a5df22
Hüseyin Burak Kocabıyık <hbkocabiyik@gmail.com>
2018-04-16 13:40:47 +0300

    21 - LikedManager Oluşturuldu

---
+++ UlakNot.BusinessLayer/Control/LikedManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UlakNot.BusinessLayer.Abstract;
+using UlakNot.DataLayer.EntityFramework;
+using UlakNot.Entity;
+
+namespace UlakNot.BusinessLayer.Control
+{
+    internal class LikedManager : ManagerBase<UnLike>
+    {
+    }
+}
