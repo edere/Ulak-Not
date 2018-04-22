4c95aa4fa6d6932091dc48c1acae5c13ab658e10
Hüseyin Burak Kocabıyık <hbkocabiyik@gmail.com>
2018-04-23 02:47:25 +0300

    35 - Not Yorum Güncelleme işlemleri yapıldı.

--- UlakNot.Web/Assets/js/comments.js
+++ UlakNot.Web/Assets/js/comments.js
@@ -9,3 +9,52 @@ $(function () {
         $(modalCommentBodyId).load("/Comment/ShowNoteComments/" + noteid);
     })
 });
+
+function doComment(btn, e, commentid, spanid) {
+    var button = $(btn);
+    var mode = button.data("edit-mode");
+
+    if (e === "edit_clicked") {
+        if (!mode) {
+            button.data("edit-mode", true);
+            button.removeClass("btn-warning");
+            button.addClass("btn-success");
+            var btnSpan = button.find("span");
+            btnSpan.removeClass("glyphicon-edit");
+            btnSpan.addClass("glyphicon-ok");
+
+            $(spanid).addClass("editable");
+            $(spanid).attr("contenteditable", true);
+            $(spanid).focus();
+        }
+        else {
+            button.data("edit-mode", false);
+            button.addClass("btn-warning");
+            button.removeClass("btn-success");
+            var btnSpan = button.find("span");
+            btnSpan.addClass("glyphicon-edit");
+            btnSpan.removeClass("glyphicon-ok");
+
+            $(spanid).removeClass("editable");
+            $(spanid).attr("contenteditable", false);
+
+            var txt = $(spanid).text();
+
+            $.ajax({
+                method: "POST",
+                url: "/Comment/Edit/" + commentid,
+                data: { text: txt }
+            }).done(function (data) {
+                if (data.result) {
+                    // yorumlar partial tekrar yüklenir..
+                    $(modalCommentBodyId).load("/Comment/ShowNoteComments/" + noteid);
+                }
+                else {
+                    alert("Yorum güncellenemedi.");
+                }
+            }).fail(function () {
+                alert("Sunucu ile bağlantı kurulamadı.");
+            });
+        }
+    }
+}
--- UlakNot.Web/Controllers/CommentController.cs
+++ UlakNot.Web/Controllers/CommentController.cs
@@ -13,7 +13,7 @@ namespace UlakNot.Web.Controllers
     public class CommentController : Controller
     {
         private NoteManager noteManager = new NoteManager();
-        private CommentManager commetManager = new CommentManager();
+        private CommentManager commentManager = new CommentManager();
 
         // GET: Comment
         public ActionResult Index()
@@ -36,6 +36,30 @@ namespace UlakNot.Web.Controllers
             }
 
             return PartialView("_PartialComments", note.Comments);
+        }
+
+        public ActionResult Edit(int? id, string text)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnComments comment = commentManager.Find(x => x.Id == id);
+
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            comment.Text = text;
+
+            if (commentManager.Update(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
         }
     }
 }
