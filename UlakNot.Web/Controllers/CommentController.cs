637b07442fb148301905f9e8f94b4a3c53f97996
Hüseyin Burak Kocabıyık <hbkocabiyik@gmail.com>
2018-04-23 02:29:09 +0300

    27 - Yorumlar notlara göre gösterildi.

---
+++ UlakNot.Web/Controllers/CommentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UlakNot.BusinessLayer.Control;
+using UlakNot.Entity;
+
+namespace UlakNot.Web.Controllers
+{
+    public class CommentController : Controller
+    {
+        private NoteManager noteManager = new NoteManager();
+        private CommentManager commetManager = new CommentManager();
+
+        // GET: Comment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowNoteComments(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnNotes note = noteManager.ListQueryable().Include("Comments").FirstOrDefault(x => x.Id == id);
+
+            if (note == null)
+            {
+                return HttpNotFound();
+            }
+
+            return PartialView("_PartialComments", note.Comments);
+        }
+    }
+}
