f6868be97f025c50707276e6b1df97c38584706c
erenc3 <huseyinerencomert@gmail.com>
2018-04-27 15:27:32 +0300

    Kategori İşlemleri Gerçekleştirildi - Listeleme,Silme,Düzeltme

--- UlakNot.Web/Controllers/AdministratorController.cs
+++ UlakNot.Web/Controllers/AdministratorController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using UlakNot.BusinessLayer.Control;
@@ -12,6 +13,8 @@ namespace UlakNot.Web.Controllers
 {
     public class AdministratorController : Controller
     {
+        private CategoryManager categoryManager = new CategoryManager();
+
         // GET: Administrator
         public ActionResult Login()
         {
@@ -48,6 +51,112 @@ namespace UlakNot.Web.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View(categoryManager.List());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnCategories category = categoryManager.Find(x => x.Id == id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UnCategories category)
+        {
+            ModelState.Remove("UpdatedDate");
+            ModelState.Remove("CreatedDate");
+            ModelState.Remove("UpdatedUserName");
+            if (ModelState.IsValid)
+            {
+                categoryManager.Insert(category);
+
+                return RedirectToAction("Category");
+            }
+
+            return View(category);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnCategories category = categoryManager.Find(x => x.Id == id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UnCategories category)
+        {
+            ModelState.Remove("UpdatedDate");
+            ModelState.Remove("CreatedDate");
+            ModelState.Remove("UpdatedUserName");
+            if (ModelState.IsValid)
+            {
+                UnCategories cat = categoryManager.Find(x => x.Id == category.Id);
+                cat.Name = category.Name;
+                cat.Description = category.Description;
+
+                categoryManager.Update(cat);
+
+                return RedirectToAction("Category");
+            }
+            return View(category);
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnCategories category = categoryManager.Find(x => x.Id == id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UnCategories category = categoryManager.Find(x => x.Id == id);
+            categoryManager.Delete(category);
+            return RedirectToAction("Category");
         }
     }
 }
