c4b78ee82cd91126a919544bf41f62acb67c4490
Hüseyin Burak Kocabıyık <hbkocabiyik@gmail.com>
2018-04-27 11:51:12 +0300

    CategoryManager Oluşturuldu

--- UlakNot.Web/Controllers/CategoryController.cs
+++ UlakNot.Web/Controllers/CategoryController.cs
@@ -1,15 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using UlakNot.BusinessLayer.Control;
 using UlakNot.Entity;
+using UlakNot.Web.Models;
 
 namespace UlakNot.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private CategoryManager categoryManager = new CategoryManager();
+
         // GET: Category
+        public ActionResult Index()
+        {
+            return View(categoryManager.List());
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnCategories category = categoryManager.Find(x => x.Id == id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // GET: Category/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UnCategories category)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryManager.Insert(category);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnCategories category = categoryManager.Find(x => x.Id == id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UnCategories category)
+        {
+            ModelState.Remove("UpdatedDate");
+            ModelState.Remove("CreatedDate");
+            ModelState.Remove("UpdatedUserName");
+            if (ModelState.IsValid)
+            {
+                UnCategories cat = categoryManager.Find(x => x.Id == category.Id);
+                cat.Name = category.Name;
+                cat.Description = category.Description;
+
+                categoryManager.Update(cat);
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        // GET: Category/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            UnCategories category = categoryManager.Find(x => x.Id == id.Value);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Category/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UnCategories category = categoryManager.Find(x => x.Id == id);
+            categoryManager.Delete(category);
+            return RedirectToAction("Index");
+        }
     }
 }
